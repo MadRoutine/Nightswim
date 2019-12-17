c2e49d6872f386ab4c1c858a7a80e9873795d5c5
walterjohan <info@madroutine.com>
2019-12-17 14:02:54 +0100

    added

---
+++ story/render.js
@@ -0,0 +1,9 @@
+let pixies = {
+    pixi_001: {
+        enter: function () {
+            console.log("Entering PIXI stage");
+        },
+        exit: function () {},
+        scene: function (tag) {}
+    }
+};
